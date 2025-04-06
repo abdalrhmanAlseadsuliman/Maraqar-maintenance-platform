387b29f807016c8860261206051e5301f29164c1
abdulhameed <abdulhameednabhan955@gmail.com>
2025-04-06 16:17:03 +0300

    custom text for notify

---
+++ lang/vendor/filament-notifications/en/database.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'modal' => [
+
+        'heading' => 'الاشعارات',
+
+        'actions' => [
+
+            'clear' => [
+                'label' => 'مسح الاشعارات',
+            ],
+
+            'mark_all_as_read' => [
+                'label' => 'تعيين كمقروؤة',
+            ],
+
+        ],
+
+        'empty' => [
+            'heading' => 'No notifications',
+            'description' => 'Please check again later.',
+        ],
+
+    ],
+
+];
