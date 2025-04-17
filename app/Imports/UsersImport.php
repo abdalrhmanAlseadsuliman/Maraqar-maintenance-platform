9d5b213792364d8c5c9843a4ce4268695fdb9a1e
abdulhameed <abdulhameednabhan955@gmail.com>
2025-04-17 10:11:12 +0300

    dev

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -4,10 +4,19 @@ namespace App\Imports;
 
 use App\Models\Property;
 use App\Models\User;
+use Illuminate\Support\Str; 
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class UsersImport implements ToModel
 {
+    function parseDate($value)
+    {
+        try {
+            return \Carbon\Carbon::parse($value)->format('Y-m-d');
+        } catch (\Exception $e) {
+            logger()->error("Invalid date: $value");
+            return null;
+        }}
     private $isFirstRow = true;
     /**
     * @param array $row
@@ -48,31 +57,27 @@ class UsersImport implements ToModel
             $this->isFirstRow = false;
             return null; // تجاهل السطر الأول
         }
-
-        // return new User([
-        //     'name'     => $row[0],
-        //     'email'    => $row[1],
-        //     'password' => $row[2],
-        // ]);
-
+    
+        // توليد بريد إلكتروني عشوائي
+        $randomEmail = Str::random(10) . '@example.com';
+    
         $user = User::create([
             'name' => $row[0],
-            'email' => 'ewen@gmail.com',
-            'password' => $row[2],
-             'phone'=>$row[6],
+            'email' => $randomEmail, // استخدم البريد العشوائي هنا
+            'password' => $row[2] ?? bcrypt('default_password'), // تأكد أن كلمة المرور صالحة
+            'phone' => $row[6] ?? null,
         ]);
-
-        
+    
         Property::create([
             'user_id' => $user->id,
             'name' => $row[3],
             'plan_number' => $row[3],
-            'sale_date'=>$row[9],
-            'property_number'=>$row[3],
-            'title_deed_number'=>$row[4],
-            'land_piece_number'=>$row[3],
+            'sale_date' => $this->parseDate($row[9]),
+            'property_number' => $row[3],
+            'title_deed_number' => $row[4],
+            'land_piece_number' => $row[3],
         ]);
-
+    
         return $user;
     }
     
