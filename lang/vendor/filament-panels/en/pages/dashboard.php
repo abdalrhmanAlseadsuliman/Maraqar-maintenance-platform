7b3ae5ae7881ba73b6664f31cb8f11624b5726c2
abdulhameed <abdulhameednabhan955@gmail.com>
2025-05-05 11:18:12 +0300

    add some translation

---
+++ lang/vendor/filament-panels/en/pages/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'title' => 'لوحة التحكم',
+
+    'actions' => [
+
+        'filter' => [
+
+            'label' => 'Filter',
+
+            'modal' => [
+
+                'heading' => 'Filter',
+
+                'actions' => [
+
+                    'apply' => [
+
+                        'label' => 'Apply',
+
+                    ],
+
+                ],
+
+            ],
+
+        ],
+
+    ],
+
+];
