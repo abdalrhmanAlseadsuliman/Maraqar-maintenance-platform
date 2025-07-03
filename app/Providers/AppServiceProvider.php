35320b3765877166358777f0e360b62ce87f656e
abdalrhmanAlseadsuliman <aboodsuliman1999@gmail.com>
2025-07-04 02:48:54 +0300

    update user and admin with mainfeast

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -75,8 +75,8 @@ class AppServiceProvider extends ServiceProvider
             <meta name="apple-mobile-web-app-title" content="مار العقارية">
             <meta name="description" content="منصة مار العقارية الرئيسية">
             <link rel="manifest" href="/manifest.json">
-            <link rel="icon" href="/images/icon-192x192.png">
-            <link rel="apple-touch-icon" href="/images/icon-192x192.png">
+            <link rel="icon" href="/images/white-logo.webp">
+            <link rel="apple-touch-icon" href="/images/white-logo.webp">
             <script>
                 if ("serviceWorker" in navigator) {
                     window.addEventListener("load", () => {
@@ -97,8 +97,8 @@ class AppServiceProvider extends ServiceProvider
             <meta name="apple-mobile-web-app-title" content="مار العقارية إدارة">
             <meta name="description" content="لوحة تحكم الإدارة">
             <link rel="manifest" href="/manifest-admin.json">
-            <link rel="icon" href="/images/icon-admin-192x192.png">
-            <link rel="apple-touch-icon" href="/images/icon-admin-192x192.png">
+            <link rel="icon" href="/images/white-logo.webp">
+            <link rel="apple-touch-icon" href="/images/white-logo.webp">
             <script>
                 if ("serviceWorker" in navigator) {
                     window.addEventListener("load", () => {
@@ -119,8 +119,8 @@ class AppServiceProvider extends ServiceProvider
             <meta name="apple-mobile-web-app-title" content="مار العقارية مستخدم">
             <meta name="description" content="لوحة تحكم المستخدم">
             <link rel="manifest" href="/manifest-user.json">
-            <link rel="icon" href="/images/icon-user-192x192.png">
-            <link rel="apple-touch-icon" href="/images/icon-user-192x192.png">
+            <link rel="icon" href="/images/white-logo.webp">
+            <link rel="apple-touch-icon" href="/images/white-logo.webp">
             <script>
                 if ("serviceWorker" in navigator) {
                     window.addEventListener("load", () => {
